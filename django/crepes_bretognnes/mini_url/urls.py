d18f7050db0bf99f24196871272a6696c984356a
stevencibambo <stevencibambo@videabiz.com>
2022-06-08 19:06:27 +0700

    release update

--- django/crepes_bretognnes/mini_url/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.shortcuts import redirect
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.liste, name='url_liste'),
-    url(r'^nouveau$', views.nouveau, name='url_nouveau'),
-    url(r'^(?P<code>\w{6})/$', views.redirection, name='url_redirection'),
-]
--- django/crepes_bretognnes/store/urls.py
+++
@@ -1,8 +0,0 @@
-#import the necessary packages
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^$', views.index),
-]
